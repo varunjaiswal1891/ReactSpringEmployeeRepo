9719c00add1936d632b4a5ad0e672829cd15e1ce
Varun Jaiswal <varjaisw@cisco.com>
2024-06-02 19:25:03 +0530

    added test code 2 june

---
+++ springreactemployeeapp/src/main/java/io/javabrains/springreactemployeeapp/SpringreactemployeeappApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.springreactemployeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactemployeeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactemployeeappApplication.class, args);
+	}
+
+}
